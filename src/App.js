c49a586e5d60ffa5ae92319a095df08e0d81c538
GucciRemyBoi <renz.wilkins@live.com>
2023-08-20 15:58:06 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Chessboard from 'chessboardjsx';
+import { Chess } from 'chess.js';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fen: 'start',
+    };
+    this.game = new Chess();
+  }
+
+  handleAIMove = () => {
+    const moves = this.game.moves();
+    const randomIndex = Math.floor(Math.random() * moves.length);
+    this.game.move(moves[randomIndex]);
+  
+    this.setState({ fen: this.game.fen() });
+  };
+  
+  onDrop = ({ sourceSquare, targetSquare }) => {
+    const move = this.game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: 'q', // Default promotion to queen
+    });
+
+    if (move === null) return;
+
+    this.setState({ fen: this.game.fen() });
+
+    setTimeout(() => {
+      this.handleAIMove();
+    }, 300); // Delay AI move for better visualization
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className='title'>React Chess</h1>
+        <Chessboard position={this.state.fen} onDrop={this.onDrop} />
+      </div>
+    );
+  }
 }
 
 export default App;
